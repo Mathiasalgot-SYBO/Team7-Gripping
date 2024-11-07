ff12ba3a0e4d328f19410d10f6e2fe87031c2bd9
Mathias2139 <mathiasalgot@gmail.com>
2024-11-07 11:10:05 +0100

    Magnet Initial Setup

--- Assets/ClawController.cs
+++ Assets/ClawController.cs
@@ -6,9 +6,12 @@ public class ClawController : MonoBehaviour
 {
     public Transform clawDesiredPoint;
     public float moveSpeed = 2;
+    public Vector2 xLimits,yLimits;
     public void MoveMagnet(Vector2 movement)
     {
         clawDesiredPoint.position += ((Vector3)movement/1024f) * moveSpeed;
+        clawDesiredPoint.position = new Vector3(Mathf.Clamp(clawDesiredPoint.position.x, xLimits.x, xLimits.y),
+            Mathf.Clamp(clawDesiredPoint.position.y, yLimits.x, yLimits.y), 0);
     }
 
     public void EnableMagnet(bool enable)
